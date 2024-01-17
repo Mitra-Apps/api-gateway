9ea92074d72ae308cfdc37ab176e1a4e9feee27d
Risyal Ambiyansyah <v-risyal.ambiyansyah@btpnsyariah.com>
2024-01-17 13:41:05 +0700

    🔥 refactor(store.go): remove commented out code for getStores function
    The commented out code for the getStores function was removed as it was no longer needed and cluttered the codebase.

--- route/rest/store.go
+++ route/rest/store.go
@@ -8,22 +8,6 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-// Get Stores
-// @Summary Display all stores
-// @Description Display all stores.
-// @Tags root
-// @Accept */*
-// @Produce json
-// @Success 200 {object} pb.GetStoresResponse
-// @Router /api/v1/store [get]
-// func (r *Rest) getStores(e echo.Context) error {
-// 	StoreList, err := r.storeService.GetStores(e.Request().Context(), &pb.GetStoresRequest{})
-// 	if err != nil {
-// 		echo.NewHTTPError(http.StatusInternalServerError, err)
-// 	}
-// 	return e.JSON(http.StatusOK, StoreList)
-// }
-
 func (r *Rest) registerStoreService(e *echo.Echo) {
 	httpProxy := httputil.NewSingleHostReverseProxy(&url.URL{
 		Scheme: "http",
