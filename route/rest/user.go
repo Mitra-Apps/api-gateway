850e821726fe71e66f7986ddc8d32cb959854bfc
agrha prayogo <agrha_a152@mitrais.com>
2024-02-02 11:16:25 +0700

    add route verify resend otp

--- route/rest/user.go
+++ route/rest/user.go
@@ -1,12 +1,31 @@
 package rest
 
 import (
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
 
+	pb "github.com/Mitra-Apps/be-user-service/domain/proto/user"
+
 	"github.com/labstack/echo/v4"
 )
+
+// Get Users
+// @Summary Display all users
+// @Description Display all users.
+// @Tags root
+// @Accept */*
+// @Produce json
+// @Success 200 {object} pb.GetUsersResponse
+// @Router /api/v1/user [get]
+func (r *Rest) getUsers(e echo.Context) error {
+	userList, err := r.userService.GetUsers(e.Request().Context(), &pb.GetUsersRequest{})
+	if err != nil {
+		echo.NewHTTPError(http.StatusInternalServerError, err)
+	}
+	return e.JSON(http.StatusOK, userList)
+}
 
 func (r *Rest) registerUserService(e *echo.Group) {
 	httpProxy := httputil.NewSingleHostReverseProxy(&url.URL{
@@ -19,4 +38,6 @@ func (r *Rest) registerUserService(e *echo.Group) {
 	e.GET("/getrole", echo.WrapHandler(httpProxy))
 	e.POST("/register", echo.WrapHandler(httpProxy))
 	e.POST("/createrole", echo.WrapHandler(httpProxy))
+	e.POST("/verify-token", echo.WrapHandler(httpProxy))
+	e.POST("/resend-otp", echo.WrapHandler(httpProxy))
 }
